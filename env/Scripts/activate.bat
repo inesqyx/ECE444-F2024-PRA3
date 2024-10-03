95a3ccc19127aaaf0a71c7f4588ae6662e27199d
Ines Qian <inesqyx@gmail.com>
2024-10-03 16:13:43 -0400

    update env

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\inesq\Documents\flaskr-tdd\env
+set VIRTUAL_ENV=C:\Users\inesq\Documents\ECE444-F2024-PRA3\env
 
 if not defined PROMPT set PROMPT=$P$G
 
